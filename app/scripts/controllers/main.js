123f0a05a246ccbd97ada6f9d7493f15221e3564
Andrew Palmer <andrew.palmer.10+github.personal@gmail.com>
2016-04-01 16:54:04 -0400

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name flipbookApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the flipbookApp
+ */
+angular.module('flipbookApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
