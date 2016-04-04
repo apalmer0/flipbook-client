071929967683a15b6872cebc22bec801c66ee958
Andrew Palmer <andrew.palmer.10+github.personal@gmail.com>
2016-04-04 10:54:58 -0400

    Add sidebar and styling

---
+++ app/scripts/directives/sidebar.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name flipbookApp.directive:sidebar
+ * @description
+ * # sidebar
+ */
+angular.module('flipbookApp')
+  .directive('sidebar', function () {
+    return {
+      templateUrl: '../views/sidebar.html',
+      restrict: 'E',
+      // link: function postLink(scope, element, attrs) {
+      //   element.text('this is the sidebar directive');
+      // }
+    };
+  });
