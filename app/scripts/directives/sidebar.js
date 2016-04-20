26077838813f3d1a0fa1b7557e9b0fb1b9ce67db
Andrew Palmer <andrew.palmer.10+github.personal@gmail.com>
2016-04-20 14:53:43 -0400

    Revert changes

--- app/scripts/directives/sidebar.js
+++ app/scripts/directives/sidebar.js
@@ -8,10 +8,9 @@
  */
 angular.module('flipbookApp')
   .directive('sidebar', function () {
-    console.log('sidebar.js');
     return {
       templateUrl: '../views/sidebar.html',
-      // restrict: 'E',
+      restrict: 'E',
       // link: function postLink(scope, element, attrs) {
       //   element.text('this is the sidebar directive');
       // }
